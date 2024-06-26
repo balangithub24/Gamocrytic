79b9815cba0a86f0eaca8ed8e4959dab98f26444
balangithub24 <balangajendiran24@gmail.com>
2024-06-26 22:17:40 +0530

    ga added

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
-// import ReactGA from "react-ga4";
+import ReactGA from "react-ga4";
 import { AllPostsProvider } from "./contexts/AllPostsProvider"; // Adjust the path as necessary
 
-// ReactGA.initialize("");
+ReactGA.initialize("G-J3HF1KPR55");
 
 // Send pageview with a custom path
 // ReactGA.send({ hitType: "pageview", page: "/logiin", title: "suiii" });
